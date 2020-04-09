34d1c5af9eb6cb8aaf3e29ab167372f7d3f7d467
aman khandelwal <amank@oakstree.com>
2020-04-09 14:03:13 +0530

    opening window update

--- m.py
+++ m.py
@@ -13,38 +13,20 @@ def donothing():
     button = Button(filewin, text="Do nothing button")
     button.pack()
 
-
-
 menubar = Menu(root)
 
 filemenu = Menu(menubar, tearoff = 0)
 filemenu.add_command(label="New", command = AddQueue(root).load)
-filemenu.add_command(label = "Open", command = donothing)
-filemenu.add_command(label = "Save", command = donothing)
-filemenu.add_command(label = "Save as...", command = donothing)
-filemenu.add_command(label = "Close", command = donothing)
 filemenu.add_separator()
 filemenu.add_command(label = "Exit", command = root.quit)
 
 menubar.add_cascade(label = "File", menu = filemenu)
-
-editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label = "Undo", command = donothing)
-editmenu.add_separator()
-editmenu.add_command(label = "Cut", command = donothing)
-editmenu.add_command(label = "Copy", command = donothing)
-editmenu.add_command(label = "Paste", command = donothing)
-editmenu.add_command(label = "Delete", command = donothing)
-editmenu.add_command(label = "Select All", command = donothing)
-
-menubar.add_cascade(label = "Edit", menu = editmenu)
 
 helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label = "Help Index", command = donothing)
 helpmenu.add_command(label = "About", command = about.load)
 
 menubar.add_cascade(label = "Help", menu = helpmenu)
-
 
 # opening window
 
@@ -59,9 +41,33 @@ Label(root,text='Queue Name',width=15).grid(row=1,column=10)
 Label(root,text='Actions',width=15).grid(row=1,column=12)
 
 var = StringVar()
-label = Label(root,textvariable = var).grid(row=2,column=2)
+#label = Label(root,textvariable = var).grid(row=2,column=2)
 var.set("sample text")
 
+
+con = db.getDbCon()
+cur = con.cursor()
+cur.execute('select _rowid_,ipurl,port,virtual_host,uname,queue_name from queue_details')
+rows = cur.fetchall()
+
+rn = 2
+for row in rows:
+    sn = row[0]
+    ip = row[1]
+    port = row[2]
+    vh = row[3]
+    uname = row[4]
+    qn = row[5]
+    Label(root,text=sn).grid(row=rn,column=2)
+    Label(root,text=ip).grid(row=rn,column=3)
+    Label(root,text=port).grid(row=rn,column=4)
+    Label(root,text=vh).grid(row=rn,column=6)
+    Label(root,text=uname).grid(row=rn,column=8)
+    Label(root,text=qn).grid(row=rn,column=10)
+    Label(root,text='View size').grid(row=rn,column=12)
+    rn = rn + 1
+
+print('rows fetched')
 root.config(menu=menubar)
 root.minsize(450, 250)
 root.title('RabbitMQ Reader')
