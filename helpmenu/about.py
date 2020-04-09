a1daf5c61343d16bcc3fa7c1cbb8c2525b383e9a
aman khandelwal <amank@oakstree.com>
2020-04-09 13:10:47 +0530

    alignment fix

--- helpmenu/about.py
+++ helpmenu/about.py
@@ -8,7 +8,7 @@ class About:
     def load(self):
         window = Toplevel(self.root)
         Label(window,text='This queue reader was My First GUI based application.').grid(row=1,column=1)
-        button = Button(window, text="Close").grid(row=2,column=2)
+        button = Button(window, text="Close").grid(row=2,column=1)
         #button.pack()
         #(self.root).mainloop()
         #(self.root).withdraw()
